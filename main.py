9ab1d27f877765299d0079fa703da3c5587dd703
RAM NARESH <88476035+i-m-ram@users.noreply.github.com>
2021-09-07 11:38:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+#importing libraries 
+import pandas as pd
+import numpy as np
+
+import director
+import convert_list
+import cleaning_data
+import joining
+
+#loading dataset
+credit_df=pd.read_csv(r'E:\Projects\Movie recommendation System\DataSet\tmdb_5000_credits.csv')
+movie_df=pd.read_csv(r'E:\Projects\Movie recommendation System\DataSet\tmdb_5000_movies.csv')
+
+#movie_df.head()----(to print first five lines of data set)
+
+#credit_df.head()----(to print first five lines of data set)
+
+credit_df.columns=['id','title','cast','crew']
+
+#merging dataset
+movie_df=movie_df.merge(credit_df,on='id')
+
+#movie_df.head()----(to print first five lines of data set)
+
+#movie_df.describe()
+
+#movie_df.info()
+
+from ast import literal_eval
+features = ["cast", "crew", "keywords", "genres"]
+for feature in features:
+    movie_df[feature] = movie_df[feature].apply(literal_eval)
+#movie_df[features].head(10)
+
+#movie_df['cast'].head()----(to print first five lines of data set)
+
+
+#getting director names using module
+movie_df['Director']=movie_df['crew'].apply(director.get_director)
+
+#converting to list using module
+features = ["cast", "keywords", "genres"]
+for feature in features:
+    movie_df[feature] = movie_df[feature].apply(convert_list.get_list)
+
+movie_df['title']=movie_df['original_title']
+
+#movie_df[['title', 'cast', 'Director', 'keywords', 'genres']].head()----(to print first five lines of data set)
+
+
+#cleaning data (removing spaces) using module
+features = ['cast', 'keywords', 'Director', 'genres']
+for feature in features:
+    movie_df[feature] = movie_df[feature].apply(cleaning_data.clean_data)
+
+#movie_df['cast'].head()----(to print first five lines of data set)
+
+#joining data from columns
+movie_df['soup']=movie_df.apply(joining.create_soup, axis=1)
+#movie_df['soup'].head()----(to print first five lines of data set)
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+count_vectorizer = CountVectorizer(stop_words='english')
+count_matrix=count_vectorizer.fit_transform(movie_df['soup'])
+#print(count_matrix.shape)
+cosine_sim2 = cosine_similarity(count_matrix, count_matrix) 
+#print(cosine_sim2.shape)
+
+movie_df = movie_df.reset_index()
+indices = pd.Series(movie_df.index, index=movie_df["title"]).drop_duplicates()
+#print(indices.head())----(to print first five lines of data set)
+
+def get_recommendations(title, cosine_sim):
+    idx = indices[title]
+    similarity_scores = list(enumerate(cosine_sim[idx]))
+    similarity_scores= sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+    similarity_scores= similarity_scores[1:11]
+    # (a, b) where a is id of movie, b is similarity_scores
+    movie_indices = [ind[0] for ind in similarity_scores]
+    movies = movie_df["title"].iloc[movie_indices]
+    return movies
+
+#print(get_recommendations("Iron Man 2", cosine_sim2))
+Movie_name=str(input('Enter Movie Name'))
+print(get_recommendations(Movie_name, cosine_sim2))
